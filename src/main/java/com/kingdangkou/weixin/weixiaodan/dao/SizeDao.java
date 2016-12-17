6c0b75ec65181f5a793c78aaa25af126de66f6d3
dongyuelsqm <dongyuelsqm@gmail.com>
2016-12-17 21:16:55 +0800

    add size controller and service

---
+++ src/main/java/com/kingdangkou/weixin/weixiaodan/dao/SizeDao.java
@@ -0,0 +1,11 @@
+package com.kingdangkou.weixin.weixiaodan.dao;
+
+import com.kingdangkou.weixin.weixiaodan.entity.SizeEntity;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by dongy on 2016-12-17.
+ */
+@Component
+public class SizeDao extends BaseDaoHibernate4<SizeEntity>{
+}
---
+++ src/main/java/com/kingdangkou/weixin/weixiaodan/service/SizeService.java
@@ -0,0 +1,18 @@
+package com.kingdangkou.weixin.weixiaodan.service;
+
+import com.kingdangkou.weixin.weixiaodan.dao.SizeDao;
+import com.kingdangkou.weixin.weixiaodan.model.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by dongy on 2016-12-17.
+ */
+@Component
+public class SizeService {
+    @Autowired
+    private SizeDao sizeDao;
+    public Result add(String name){
+
+    }
+}
