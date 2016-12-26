8862ee15fabcb71998d3c668cd9f9d5831c6fd1f
dongyuelsqm <dongyuelsqm@gmail.com>
2016-12-26 23:36:16 +0800

    fix the 404 error when visit the static resource

--- src/main/java/com/kingdangkou/weixin/weixiaodan/utils/configs/OrderJsonConfig.java
+++ src/main/java/com/kingdangkou/weixin/weixiaodan/utils/configs/OrderJsonConfig.java
@@ -1,10 +1,12 @@
 package com.kingdangkou.weixin.weixiaodan.utils.configs;
 
 import net.sf.json.JsonConfig;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by dongy on 2016-12-26.
  */
+@Component
 public class OrderJsonConfig extends JsonConfig{
     public OrderJsonConfig() {
         setExcludes(new String[]{"order"});
