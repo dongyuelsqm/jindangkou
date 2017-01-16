a2e375066a557ab3aaaf22be7b8e4a54611a7ffc
cailuwei <cailuwei@chinamobile.com>
2017-01-16 10:40:10 +0800

    sql script

---
+++ src/main/test_resources/dropdata.sql
@@ -0,0 +1,20 @@
+/* 2017-1-16 create */
+drop table products;
+drop table product_quantity;
+drop table orders;
+drop table sub_orders;
+drop table addresses;
+drop table department;
+drop table inventory;
+drop table admin;
+drop table labels;
+drop table product_label_links;
+drop table colors;
+drop table sizes;
+drop table departments;
+drop table collections;
+drop table shoping_cart;
+drop table notifications;
+drop table customers;
+
+/* 2017-1- update */
