d84a72355a93daf15f652fef01c172adc4be3003
dongyuelsqm <dongyuelsqm@gmail.com>
2017-03-08 23:48:26 +0800

    修改login校验错误

--- src/main/java/com/kingdangkou/weixin/weixiaodan/controller/LoginController.java
+++ src/main/java/com/kingdangkou/weixin/weixiaodan/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.kingdangkou.weixin.weixiaodan.controller;
 
 import com.kingdangkou.weixin.weixiaodan.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,12 +13,13 @@ import java.io.IOException;
 /**
  * Created by dongy on 2016-11-30.
  */
-@RequestMapping("/website/login")
+@RequestMapping("/website")
+@Controller
 public class LoginController {
 
     @Autowired
     private LoginService loginService;
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     public void login(@RequestParam("username") String username,
                       @RequestParam("password") String password, HttpServletResponse response) throws IOException {
         boolean result = loginService.check(username, password);
