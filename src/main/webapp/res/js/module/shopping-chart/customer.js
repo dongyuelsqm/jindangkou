0d6cfab654472b94410027d568d5abfa59cf4476
cailuwei <cailuwei@chinamobile.com>
2017-03-23 17:48:14 +0800

    user-info add

--- src/main/webapp/res/js/module/shopping-chart/customer.js
+++ src/main/webapp/res/js/module/shopping-chart/customer.js
@@ -14,6 +14,10 @@ define(function(require, exports, module) {
     require('jquery-validate');
     require('jquery-validate-add');
 
+    var template = util.template,
+        SelectView = form.SelectView,
+        cityMap = require('module/default/cityMap');
+
     var upload_error = {
         '-100': '发生通用错误',
         '-200': 'http网络错误',
@@ -28,41 +32,60 @@ define(function(require, exports, module) {
         '-702': '文件大小超过了plupload所能处理的最大值'
     };
 
+    var urls = {
+        add: G.contextPath + 'website/storekeeperinfo/add',
+        upload: G.contextPath + 'file/upload'
+    };
+
     var validates = {
         rules: {
-            userName: {required: true},
-            userMobile: {
+            username: {required: true},
+            phone: {
                 required: true,
                 mobile: true
             },
-            qq: {required: true},
-            userEmail: {
+            QQ: {required: true},
+            email: {
                 required: true,
                 email: true
             },
-            shopName: {required: true},
+            storeName: {required: true},
             // pictures: {required: true},
-            address: {required: true},
-            telephone: {
+            // address: {required: true},
+            province: {required: true},
+            city: {required: true},
+            district: {required: true},
+            detail: {
+                required: true,
+                maxlength: 200
+            },
+            tel: {
                 required: true,
                 contactNum: true
             }
         },
         messages: {
-            userName: {required: '请输入用户名'},
-            userMobile: {
+            username: {required: '请输入用户名'},
+            phone: {
                 required: '请输入手机号码',
                 mobile: '请输入正确格式的手机号码'
             },
-            qq: {required: '请选择qq号码'},
-            userEmail: {
+            QQ: {required: '请选择qq号码'},
+            email: {
                 required: '请输入常用邮箱',
                 mobile: '请输入正确格式的电子邮箱'
             },
-            shopName: {required: '请输入店铺名称'},
+            storeName: {required: '请输入店铺名称'},
             // pictures: {required: true},
-            address: {required: '请输入发货地址'},
-            telephone: {
+            // address: {required: '请输入发货地址'},
+            province: {required: '请选择省份'},
+            city: {required: '请选择城市'},
+            district: {required: '请选择街道'},
+            detail: {
+                required: '请输入详细公司地址',
+                maxlength: '请输入200位以内字符'
+            },
+            tel: {
                 required: '请输入服务电话',
                 contactNum: '请输入正确格式的服务电话'
             }
@@ -78,18 +101,32 @@ define(function(require, exports, module) {
         events: {
             'click button[role="btn-submit"]': 'submit',
             'click img[role="upload-img"]': 'uploadImage',
+            'change #province_select>li>a': 'selectProvince',
+            'change #city_select>li>a': 'selectCity',
+            'change #district_select>li>a': 'selectDistrict'
         },
         initialize: function(opstions){
-            this.$checkboxs = [];
+            var _this = this;
+            _this.$checkboxs = [];
+            _this.select_map = {};
 
-            this.initImgUpload();
-            this.$el.validate(validates);
+            _this.initImgUpload();
+
+            _.each(this.$('.dropdown'), function(item, index){
+                _this.select_map[item.id] = new SelectView({
+                    el: $(item),
+                    init_data: true
+                });
+            });
+            this.initProvince();
+
+            this.validator = this.$el.validate(validates);
         },
         initImgUpload: function(){
             var _this = this, uploader = {};
             this.uploader_img = new plupload.Uploader({
                 browse_button: document.getElementById('btn-img'),
-                url: 'file/upload',
+                url: urls.upload,
                 flash_swf_url: G.contextPath + '/res/js/plugins/plupload/Moxie.swf',
                 silverlight_xap_url : G.contextPath + '/res/js/plugins/plupload/Moxie.xap',
                 multi_selection: false,
@@ -141,6 +178,106 @@ define(function(require, exports, module) {
 
             this.uploader_img.init();
         },
+        initProvince: function(){
+            var _this = this;
+            _this.selectType = 'province';
+            _this.selectData = cityMap.provinceMap || [];
+            _this.setSelectData();
+        },
+        setSelectData: function(){
+            var _this = this;
+            if(_this.selectData.length > 0){
+                var html = '';
+
+                var val = _this.$('#' + _this.selectType).val(),
+                    id = 0;
+                _.each(_this.selectData, function(item, index){
+                    if(_this.selectType === 'province'){
+                        html += '<li><a href="javascript:" data-val="' + item.ProID + '">' + item.name + '</a></li>';
+                        _this.provinceId = (val === item.name ? item.ProID : '');
+                    }
+                    if(_this.selectType === 'city'){
+                        html += '<li><a href="javascript:" data-val="' + item.CityID + '">' + item.name + '</a></li>';
+                        _this.cityId = (val === item.name ? item.CityID : '');
+                    }
+                    if(_this.selectType === 'district'){
+                        html += '<li><a href="javascript:" data-val="' + item.Id + '">' + item.name + '</a></li>';
+                        _this.districtId = (val === item.name ? item.Id : '');
+                    }
+                });
+                _this.$('#' + _this.selectType + '_dropdown').find('ul.ul-dropdown').html(html);
+            }
+        },
+        'selectProvince': function(ev){
+            var _this = this,
+                $this = {},
+                provinceId = 0,
+                array = [];
+
+            if(typeof ev === 'object'){
+                $this = $(ev.currentTarget);
+                provinceId = $this.data('val');
+            }else{
+                provinceId = ev;
+                this.select_map['province_dropdown'].setText(provinceId);
+            }
+
+            if(provinceId !== ''){
+                _.each(cityMap.cityMap, function(item, index){
+                    if(item.ProID === provinceId){
+                        array.push(item);
+                    }
+                });
+
+                _this.selectType = 'city';
+                _this.selectData = array;
+                _this.setSelectData();
+                _this.selectCity(_this.cityId || _this.selectData[0].CityID);
+            }
+        },
+        'selectCity': function(ev){
+            var _this = this,
+                $this = {},
+                cityId = 0,
+                array = [];
+
+            this.cityId = null;
+            if(typeof ev === 'object'){
+                $this = $(ev.currentTarget);
+                cityId = $this.data('val');
+            }else{
+                // $this = _this.$('#city_select');
+                cityId = ev;
+                this.select_map['city_dropdown'].setText(cityId);
+            }
+
+            if(cityId !== ''){
+                _.each(cityMap.districtMap, function(item, index){
+                    if(item.CityID === cityId){
+                        array.push(item);
+                    }
+                });
+
+                _this.selectType = 'district';
+                _this.selectData = array;
+                _this.setSelectData();
+                _this.selectDistrict(_this.districtId || _this.selectData[0].Id);
+            }
+        },
+        'selectDistrict': function(ev){
+            var $this = {},
+                districtId = 0;
+
+            this.districtId = null;
+            if(typeof ev === 'object'){
+                $this = $(ev.currentTarget);
+                districtId = $this.data('val');
+            }else{
+                // $this = this.$('#district_select');
+                districtId = ev;
+                this.select_map['district_dropdown'].setText(districtId);
+            }
+        },
         'uploadImage': function(ev){
             var $this = $(ev.currentTarget),
                 _this = this;
@@ -151,14 +288,42 @@ define(function(require, exports, module) {
             var $this = $(ev.currentTarget),
                 _this = this;
             console.log(_this.$el.serializeArray());
-            if(this.$el.validate()){}
-            $.ajax({
-                url: G.contextPath + 'customer/add',
-                data: _this.$el.serializeArray(),
-                success: function(rsp){
-                    console.log(rsp);
-                }
-            });
+            if(this.validator.form()) {
+                var param = _this.$el.serializeObject(),
+                    obj = {'openID': 'sss'};
+
+                _.each(param, function (value, key) {
+                    switch (key) {
+                        case 'city':
+                            obj['cityName'] = value;
+                            delete param[key];
+                            break;
+                        case 'detail':
+                            obj['expAreaName'] = value;
+                            delete param[key];
+                            break;
+                        case 'district':
+                            obj['address'] = value;
+                            delete param[key];
+                            break;
+                        case 'province':
+                            obj['provinceName'] = value;
+                            delete param[key];
+                            break;
+                        default:
+                            break;
+                    }
+                });
+                param.address = JSON.stringify(obj);
+                param.picture = 'picture';
+                $.ajax({
+                    url: urls.add,
+                    data: param,
+                    success: function (rsp) {
+                        console.log(rsp);
+                    }
+                });
+            }
         }
     });
 
